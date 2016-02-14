ba97867df5437bd6d832c28e4e23617d8a73f9a5
Rob O'Dwyer <odwyerrob@gmail.com>
2016-02-14 14:57:31 -0800

    clean up commands

--- commands.go
+++ commands.go
@@ -2,26 +2,25 @@ package main
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 )
 
-type jsonData map[string]string
+type JSON map[string]string
 
-func respond(res http.ResponseWriter, obj jsonData) {
+func respond(res http.ResponseWriter, obj JSON) {
 	res.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(res).Encode(obj)
 }
 
 func handleInfo(res http.ResponseWriter, req *http.Request) {
-	respond(res, jsonData{
+	respond(res, JSON{
 		"color": "#ff0000",
 		"head":  "https://golang.org/doc/gopher/gopherbw.png",
 	})
 }
 
 func handleStart(res http.ResponseWriter, req *http.Request) {
-	respond(res, jsonData{
+	respond(res, JSON{
 		"taunt": "battlesnake-go!",
 	})
 }
@@ -29,51 +28,39 @@ func handleStart(res http.ResponseWriter, req *http.Request) {
 func handleMove(res http.ResponseWriter, req *http.Request) {
 	data, err := NewMoveRequest(req)
 	if err != nil {
-		respond(res, jsonData{"move": "north", "taunt": "can't parse this!"})
+		respond(res, JSON{"move": "north", "taunt": "can't parse this!"})
 		return
 	}
 
 	snake := data.GetSnake(SNAKE_ID)
 	if snake == nil {
-		respond(res, jsonData{"move": "north", "taunt": "snake not found!"})
+		respond(res, JSON{"move": "north", "taunt": "snake not found!"})
 		return
 	}
-	location := snake.Head()
-
-	log.Printf("Currently at %d,%d", location.X, location.Y)
 
 	board := NewBoard(data)
+	location := snake.Head()
 
-	chosenDirection := "north"
+	var chosenDirection string
 
-	directions := []struct {
-		name string
-		x    int
-		y    int
-	}{
-		{"west", location.X - 1, location.Y},
-		{"east", location.X + 1, location.Y},
-		{"north", location.X, location.Y - 1},
-		{"south", location.X, location.Y + 1},
+	if tile := board.GetTile(location.X, location.Y-1); tile == EMPTY {
+		chosenDirection = "north"
+	} else if tile := board.GetTile(location.X, location.Y+1); tile == EMPTY {
+		chosenDirection = "south"
+	} else if tile := board.GetTile(location.X-1, location.Y); tile == EMPTY {
+		chosenDirection = "west"
+	} else if tile := board.GetTile(location.X+1, location.Y); tile == EMPTY {
+		chosenDirection = "east"
+	} else {
+		chosenDirection = "north"
 	}
 
-	for _, direction := range directions {
-		tile := board.GetTile(direction.x, direction.y)
-
-		log.Printf("Tile %d,%d is %s", direction.x, direction.y, tile)
-
-		if tile == EMPTY {
-			chosenDirection = direction.name
-			break
-		}
-	}
-
-	respond(res, jsonData{
+	respond(res, JSON{
 		"move":  chosenDirection,
 		"taunt": "go go go!",
 	})
 }
 
 func handleEnd(res http.ResponseWriter, req *http.Request) {
-	respond(res, jsonData{})
+	respond(res, JSON{})
 }
